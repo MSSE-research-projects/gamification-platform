76f5ccde4e2bfe41188c8f3990e5bfcd944360cc
Sijie Luo <sijieluo@andrew.cmu.edu>
2022-06-10 22:41:10 -0700

    feat(model): add CustomUser model

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'app.gamification',
 ]
 
 MIDDLEWARE = [
@@ -109,6 +111,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'gamification.CustomUser'
 
 
 # Internationalization
