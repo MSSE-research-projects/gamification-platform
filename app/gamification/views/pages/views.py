321466a91778fd0fb1a653797756e043d52e4847
yunshan111 <65985986+yunshan111@users.noreply.github.com>
2022-06-14 14:56:16 -0700

    feat: add signin function&fix signin templete

--- app/gamification/views/pages/views.py
+++ app/gamification/views/pages/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import login
 from django.shortcuts import redirect, render
-
+from django.contrib.auth.forms import AuthenticationForm
 from ...forms import SignUpForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 
 def signup(request):
@@ -21,10 +23,21 @@ def signup(request):
 def dashboard(request):
     return render(request, 'dashboard.html')
 
-def signin(request):
-    if request.method == 'POST':
-        return redirect('dashboard')
-    else:
-        form = SignUpForm(label_suffix='')
 
-    return render(request, 'signin.html', {'form': form})
+def signin(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect('dashboard')
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="signin.html", context={"login_form":form})
