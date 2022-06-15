b0fc6ac155fc9b69d7fbe05c593149d4c0684e57
Sijie Luo <sijieluo@andrew.cmu.edu>
2022-06-14 21:54:52 -0700

    fix: fix singin button

--- app/gamification/views/pages/views.py
+++ app/gamification/views/pages/views.py
@@ -25,19 +25,22 @@ def dashboard(request):
 
 
 def signin(request):
-	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				messages.info(request, f"You are now logged in as {username}.")
-				return redirect('dashboard')
-			else:
-				messages.error(request,"Invalid username or password.")
-		else:
-			messages.error(request,"Invalid username or password.")
-	form = AuthenticationForm()
-	return render(request=request, template_name="signin.html", context={"login_form":form})
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect('dashboard')
+            else:
+                messages.error(request, "Invalid username or password.")
+        # else:
+        #     messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+
+    print(form.errors)
+    return render(request=request, template_name="signin.html", context={"login_form": form})
