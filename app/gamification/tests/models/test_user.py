cbd4da93bfd3a397995b634ca01ad9fe3f3af1b1
Sijie Luo <sijieluo@andrew.cmu.edu>
2022-06-16 22:03:15 -0700

    test: add tests for CustomUserManager

--- app/gamification/tests/models/test_user.py
+++ app/gamification/tests/models/test_user.py
@@ -1,6 +1,36 @@
 from django.test import TestCase
 
-from app.gamification.models import CustomUser
+from app.gamification.models import CustomUser, CustomUserManager
+
+
+class CustomUserManagerTest(TestCase):
+
+    def setUp(self):
+        self.data = {
+            'andrew_id': 'alice',
+            'email': 'alice@example.com',
+            'password': 'arbitary-password',
+        }
+
+    def test_create_user(self):
+        # Arrange
+        manager = CustomUser.objects
+        # Act
+        manager.create_user(**self.data)
+        # Assert
+        query_set = CustomUser.objects.filter(andrew_id='alice')
+        self.assertTrue(query_set.exists())
+
+    def test_create_superuser(self):
+        # Arrange
+        manager = CustomUser.objects
+        # Act
+        user = manager.create_superuser(**self.data)
+        # Assert
+        query_set = CustomUser.objects.filter(andrew_id='alice')
+        self.assertTrue(query_set.exists())
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
 
 
 class CustomUserTest(TestCase):
