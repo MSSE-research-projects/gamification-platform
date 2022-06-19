7a86c615c8259fd17b15446796c39bb6b3fcc965
Sijie Luo <sijieluo@andrew.cmu.edu>
2022-06-19 01:25:38 -0700

    feat: change UserDetail permissions

--- app/gamification/views/api/user.py
+++ app/gamification/views/api/user.py
@@ -5,18 +5,13 @@ from app.gamification.models import CustomUser
 from app.gamification.serializers import UserSerializer
 
 
-class IsAdminOrSelfOrReadOnly(permissions.BasePermission):
+class IsAdminOrSelf(permissions.BasePermission):
     '''
     Custom permission to only allow users to edit information of itself.
     Admin users are allowed to edit information of all users.
     '''
 
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we will always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
         # Write permissions are only allowed to the user itself or admin users
         return bool(request.user.is_staff or request.user == obj)
 
@@ -31,4 +26,4 @@ class UserDetail(generics.RetrieveUpdateAPIView):
     queryset = CustomUser.objects.all()
     lookup_field = 'andrew_id'
     serializer_class = UserSerializer
-    permission_classes = [IsAdminOrSelfOrReadOnly]
+    permission_classes = [IsAdminOrSelf]
