865c2f9fab6499675927b2f70c472c4a083663aa
AlexHe0131 <alex0131@outlook.com>
2022-07-17 01:15:13 -0700

    feat: survey views
    Co-Authored-By: Jiadong Zhang <65985986+yunshan111@users.noreply.github.com>

--- app/gamification/views/pages/survey_views.py
+++ app/gamification/views/pages/survey_views.py
@@ -1,11 +1,11 @@
+from re import template
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
-from django.urls import reverse
 
 
 from app.gamification.decorators import admin_required, user_role_check
 from app.gamification.forms import AddSurveyForm
-from app.gamification.models import Assignment, Course, CustomUser, Registration, Team, Membership, SurveySection, SurveyTemplate, FeedbackSurvey, Question
+from app.gamification.models import Assignment, Registration, SurveySection, FeedbackSurvey, Question
 
 from .section_question import SECTION_QUESTION
 
@@ -19,27 +19,36 @@ def add_survey(request, course_id, assignment_id):
     elif request.method == 'POST':
         form = AddSurveyForm(request.POST, label_suffix='')
         if form.is_valid():
-            form.save()
-        return render(request, 'section.html', {'course_id': course_id, 'assignment_id': assignment_id, 'form': form})
+            template = form.save()
+        assignment = Assignment.objects.get(id=assignment_id)
+        feedback_survey = FeedbackSurvey(
+            assignment=assignment, template=template)
+        feedback_survey.save()
+        return redirect('survey_list', course_id, assignment_id)
     else:
-        return render(request, 'section.html', {'course_id': course_id, 'assignment_id': assignment_id})
+        return render(request, 'survey_detail.html', {'course_id': course_id, 'assignment_id': assignment_id})
 
 
 @login_required
 @user_role_check(user_roles=[Registration.UserRole.Instructor, Registration.UserRole.TA])
 def survey_list(request, course_id, assignment_id):
     if request.method == 'GET':
-        feedback_survey = FeedbackSurvey.objects.get(
-            assignment__pk=assignment_id)
-        survey_template = feedback_survey.survey_template
-        survey_sections = SurveySection.objects.filter(
-            survey_template=survey_template)
+        assignment = Assignment.objects.get(id=assignment_id)
+        feedback_survey = FeedbackSurvey.objects.filter(
+            assignment=assignment)
+        if(len(feedback_survey) == 1):
+            feedback_survey = feedback_survey[0]
+            survey_template = feedback_survey.template
+            survey_sections = SurveySection.objects.filter(
+                template=survey_template)
+        else:
+            survey_sections = []
         survey = {}
         for survey_section in survey_sections:
             survey_questions = Question.objects.filter(
-                survey_section=survey_section)
+                section=survey_section)
             survey[survey_section] = list(survey_questions)
-        return render(request, 'survey_list.html', {'course_id': course_id, 'assignment_id': assignment_id, 'survey': survey, 'section_question': SECTION_QUESTION})
+        return render(request, 'survey_detail.html', {'course_id': course_id, 'assignment_id': assignment_id, 'survey': survey, 'section_question': SECTION_QUESTION})
     else:
         return redirect('survey_list', course_id, assignment_id)
 
@@ -47,26 +56,23 @@ def survey_list(request, course_id, assignment_id):
 @login_required
 @user_role_check(user_roles=[Registration.UserRole.Instructor, Registration.UserRole.TA])
 def add_section(request, course_id, assignment_id):
-    def get_all_sections(self, assignment_id):
-        # feedback_surveys = FeedbackSurvey.objects.filter(assignment=assignment_id, template=)
-        # sections = Section.objects.filter(assignment=assignment_id)
-        # return SurveySection.objects.all()
-        pass
-
-    if request.method == 'GET':
-        sections = get_all_sections(request, assignment_id)
-        section_title_list = list(SECTION_QUESTION.keys())
-        context = {
-            'sections': sections,
-            'section_title_list': section_title_list,
-            'course_id': course_id,
-            'assignment_id': assignment_id,
-        }
-        return render(request, 'section.html', context)
-    elif request.method == 'POST':
-        return render(request, 'question.html', {'course_id': course_id, 'assignment_id': assignment_id})
+    if request.method == 'POST':
+        section_title = request.POST.get('section_title')
+        optional = request.POST.get('optional')
+        is_required = False if optional == 'on' else True
+        assignment = Assignment.objects.get(id=assignment_id)
+        feedback_survey = FeedbackSurvey.objects.filter(
+            assignment=assignment)
+        # TODO FIX LENGTH
+        if(len(feedback_survey) >= 1):
+            feedback_survey = feedback_survey[0]
+            survey_template = feedback_survey.template
+            survey_section = SurveySection(
+                title=section_title, is_required=is_required, template=survey_template)
+            survey_section.save()
+        return redirect('survey_list', course_id, assignment_id)
     else:
-        return render(request, 'question.html', {'course_id': course_id, 'assignment_id': assignment_id})
+        return render(request, 'survey_detail.html', {'course_id': course_id, 'assignment_id': assignment_id})
 
 
 @login_required
