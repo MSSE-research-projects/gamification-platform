38b27fd2af933da4f05f2ad92a64513324d3ebf7
yunshan111 <65985986+yunshan111@users.noreply.github.com>
2022-07-31 17:13:33 -0700

    feat: answer API

--- app/gamification/serializers/answer.py
+++ app/gamification/serializers/answer.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 
-from app.gamification.models import SurveyTemplate, SurveySection, Answer, ArtifactReview
+from app.gamification.models import Answer, ArtifactReview, ArtifactFeedback
 from app.gamification.models.option_choice import OptionChoice
 from app.gamification.models.question import Question
+
 
 
 class AnswerSerializer(serializers.ModelSerializer):
@@ -10,8 +11,15 @@ class AnswerSerializer(serializers.ModelSerializer):
         model = Answer
         fields = ['pk', 'artifact', 'questionOption', 'answerText']
 
+class ArtifactFeedbackSerializer(serializers.ModelSerializer):
+    answer = AnswerSerializer()
+    class Meta:
+        model = ArtifactFeedback
+        fields = ['pk', 'page', 'answer']
+
 
 class ArtifactReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = ArtifactReview
         fields = ['pk', 'artifact', 'regisration']
+
