6d22c86e3c7dcfb0180cedc586ae28a97030fe3f
Sijie Luo <sijieluo@andrew.cmu.edu>
2022-08-16 01:50:58 -0700

    fix: fix bugs

--- app/gamification/views/pages/views.py
+++ app/gamification/views/pages/views.py
@@ -1,16 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from app.gamification.decorators import admin_required
-from app.gamification.views.pages.artifact_views import *
-from app.gamification.views.pages.assignment_views import *
-from app.gamification.views.pages.course_views import *
-from app.gamification.views.pages.dashboard_views import *
-from app.gamification.views.pages.member_views import *
-from app.gamification.views.pages.profile_views import *
-from app.gamification.views.pages.report_views import *
-from app.gamification.views.pages.signin_views import *
-from app.gamification.views.pages.survey_views import *
-from app.gamification.views.pages.test_views import *
 
 
 @login_required
